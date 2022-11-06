4808ba61b6acd783147ac6ad8f26d7155185e5ba
DariaSteg <dariasteg@gmail.com>
2022-11-06 21:56:43 +0200

    Use types & let

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('505')
+console.log(505)
+console.log(true)
+console.log(null)
+console.log(undefined)
+
+let userFirstname;
+userFirstname = 'Bred';
+userFirstname = "O'Donel";
+userFirstname = 'O\'Donel';
+console.log(userFirstname)

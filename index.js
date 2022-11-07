2af2e8238e2ccd7312500e0fd1a5072c015ba5e5
DariaSteg <dariasteg@gmail.com>
2022-11-07 12:05:11 +0200

    Home work

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-console.log('505')
-console.log(505)
-console.log(true)
-console.log(null)
-console.log(undefined)
+let number1 =45;
+let number2 = 12;
 
-let userFirstname;
-userFirstname = 'Bred';
-userFirstname = "O'Donel";
-userFirstname = 'O\'Donel';
-console.log(userFirstname)
+console.log( number1+ number2);
+console.log( number1- number2);
+console.log( number1* number2);
+console.log( number1/ number2);
+console.log( number1% number2);
+
+let number =456;
+number+=6;
+number++;
+number/=2;
+number-=3;
+--number;
+number %=4;
+number +='qwerty';
+
+
